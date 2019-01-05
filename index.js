e21481164803ebe4d75c438a08518a1ae28b7943
AaronTeele <32844775+AaronTeele@users.noreply.github.com>
2019-01-05 00:53:13 -0500

    core functionality

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-const Math = require('./math')
+const express = require('express');
+const app = express();
+const Math = require('./math');
+const port = 3000;
 
+app.get('/math/add', (req, res) =>{
+    let query = req.query;
+    let keys = Object.keys(query)
+    let output = {
+        input: {},
+        sumString: '',
+        sum: 0,
+    }
+    console.log('query', query)
+    console.log('keys', keys)
+    
+     for (let i = 0; i < keys.length; i++) {
+         let num = parseInt(query[keys[i]])
+        output.input[keys[i]] = num;
+        output.sumString += `${num}`
+            if(i < keys.length - 1){
+                output.sumString += ' + ';
+            };
+        output.sum += num;
+     };
+
+     res.send(output)
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
